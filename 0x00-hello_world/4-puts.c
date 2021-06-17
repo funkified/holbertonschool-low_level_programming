2c37ebcb5f319944f0b1c5eabffebd4d2f001300
Sergio Vera <sergio.vera@icloud.com>
2021-06-17 15:45:13 +0000

    4puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ *
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	char *str = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return(0);
+}
