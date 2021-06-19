24f76406239d04971091583bdacd1ee309b222e7
Sergio Vera <sergio.vera@icloud.com>
2021-06-19 03:27:19 +0000

    task 2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints uppercase alphabet
+ *
+ * Return: zero (success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+
+	return (0);
+}
