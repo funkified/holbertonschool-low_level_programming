844a89114c7122b53bf72714c0321dd562626b85
Sergio Vera <sergio.vera@icloud.com>
2021-06-19 19:19:01 +0000

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in reverse
+ *
+ * Return: Alway zero
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
