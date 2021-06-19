fc3dba706d2688f5f11b9dd89d56496d368ef2c8
Sergio Vera <sergio.vera@icloud.com>
2021-06-19 03:25:38 +0000

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lower;
+
+	for(lower = 'a'; lower <= 'z'; lower++)
+	{
+		putchar(lower);
+	}
+	putchar('\n');
+	return (0);
+}
+
