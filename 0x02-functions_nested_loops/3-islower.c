e1a52a5f4ac3510e5e85692d1b0d3cee5fe19e67
Sergio Vera <sergio.vera@icloud.com>
2021-06-22 22:55:28 +0000

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _islower - check for lowercase characters
+ *
+ * @c: variable to be tested
+ * Return: 1 if c is lowercase 0, otherwise
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 97  && c <= 122)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
