4effda1f23be7b85f4ae557088e8e70bf213bac4
Sergio Vera <sergio.vera@icloud.com>
2021-06-22 23:09:20 +0000

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: variable to be tested
+ * Return: 1 if c is a letter lowercase or uppercase. 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
