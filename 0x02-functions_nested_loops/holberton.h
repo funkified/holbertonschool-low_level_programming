f24208c1ed101dfe726063d4d2c44b9389666fb6
Sergio Vera <sergio.vera@icloud.com>
2021-06-22 22:41:37 +0000

    pushing header

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int_islower(int c);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+
+#endif
