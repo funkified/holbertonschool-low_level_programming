f4080b0943b8856f21896b40a81b8e35058e93c1
Sergio Vera <sergio.vera@icloud.com>
2021-06-23 15:05:48 +0000

    fixing spaces

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,12 +12,12 @@ void print_to_98(int n)
 	if (n > 98)
 	{
 		for (; n > 98; n--)
-			printf("%d ,", n);
+			printf("%d, ", n);
 	}
 	if (n < 98)
 	{
 		for (; n < 98; n++)
-			printf("%d ,", n);
+			printf("%d, ", n);
 	}
 		printf("98\n");
 }
