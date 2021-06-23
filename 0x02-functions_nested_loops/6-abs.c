45ced300d37e55e5704f5615b68d65f9146279f1
Sergio Vera <sergio.vera@icloud.com>
2021-06-23 00:04:48 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _abs - computes the absolute value of an interger
+ *@n: tested interger
+ *Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
