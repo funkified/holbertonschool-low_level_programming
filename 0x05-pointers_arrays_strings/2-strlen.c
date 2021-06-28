b8bc830547c2bd0fcdc50eeec727795ab4e91d89
Sergio Vera <sergio.vera@icloud.com>
2021-06-28 23:05:54 +0000

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strlen - calculates the length of a string
+ * @s: points to string to be measure
+ * Return: number of chars of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
