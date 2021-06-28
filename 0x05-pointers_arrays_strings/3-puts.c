fedf40f6efa85ea7235817e5532b1222d0a5c7e9
Sergio Vera <sergio.vera@icloud.com>
2021-06-28 23:40:33 +0000

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints a string
+ * @str: pointer to string
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
