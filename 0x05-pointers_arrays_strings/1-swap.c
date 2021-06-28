ccccdb5ceb52a62b75a3339be8b1d72a30ce1208
Sergio Vera <sergio.vera@icloud.com>
2021-06-28 19:40:31 +0000

    swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swap the value of two intergers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = 98;
+}
