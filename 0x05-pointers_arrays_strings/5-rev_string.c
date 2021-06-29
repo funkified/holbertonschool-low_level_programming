7b62853b333a60f3be08d4215b46d38f1cd52c9f
Sergio Vera <sergio.vera@icloud.com>
2021-06-29 01:17:39 +0000

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: points to string to be reverse
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int sindex, rIndex, i = 0;
+	char strcpy;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (rIndex = (i - 1); rIndex > sindex; rIndex--)
+	{
+		strcpy = s[sindex];
+		s[sindex] = s[rIndex];
+		s[rIndex] = strcpy;
+		sindex++;
+	}
+}
