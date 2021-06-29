c21b7c8611f3d77fbf1742efbe0cd03a9a80b020
Sergio Vera <sergio.vera@icloud.com>
2021-06-29 03:55:37 +0000

    6puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,10 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i++]);
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
