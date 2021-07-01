da766ec49da36c4615f28fc00e51f4f52ddc4ffe
Sergio Vera <sergio.vera@icloud.com>
2021-07-01 18:24:54 +0000

    fixing loop

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int len;
 
-	for (len = 0; s1[len] == s2[len]; len++)
+	for (len = 0; s1[len] && s2[len]; len++)
 	{
-		if (s1[len] != '\0' && s2[len] != '\0')
+		if (s1[len] != s2[len])
 		{
-			return (0);
+			return (s1[len] - s2[len]);
 		}
 	}
-	return (s1[len] - s2[len]);
+	return (0);
 }
