141844c0740dc703f4b2f1c1870ccb47b7797e2e
Sergio Vera <sergio.vera@icloud.com>
2021-07-01 15:32:30 +0000

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to be compared
+ * @s2: second string
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int len;
+
+	for (len = 0; s1[len] == s2[len]; len++)
+	{
+		if (len != '\0')
+		{
+			return (0);
+		}
+	}
+	return (s1[len] - s2[len]);
+}
