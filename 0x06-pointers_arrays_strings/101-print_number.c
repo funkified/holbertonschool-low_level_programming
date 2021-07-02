b145f1f55d6bf763b9004bdf319ed79891dcddb1
Sergio Vera <sergio.vera@icloud.com>
2021-07-02 19:20:50 +0000

    task 4 - 101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints an interger
+ * @n: variable for testing intergers
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		i = -n;
+		_putchar('-');
+	}
+	else
+	{
+		i = n;
+	}
+	if (i / 10 != 0)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
