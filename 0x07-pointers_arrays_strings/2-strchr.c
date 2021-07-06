d6f150fa5fd267da663ddca97b00cde5ddde140d
Sergio Vera <sergio.vera@icloud.com>
2021-07-06 22:35:39 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character i a string
+ * @s: pointer to string
+ * @c: character count
+ * Return: a pointere to the character in string
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	int i;
+
+	while (s[i] != '\0' && s[i] != c)
+	{
+		i++;
+	}
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+	else
+			return (0);
+}
