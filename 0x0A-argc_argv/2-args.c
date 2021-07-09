6c9c594b99cb514f67af9b176300975f222d3e4c
Sergio Vera <sergio.vera@icloud.com>
2021-07-09 18:52:56 +0000

    2args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - counts number of arguments passed
+ * @argc: arguments count
+ * @argv: vector
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n ", *argv++);
+	}
+	return (0);
+}
