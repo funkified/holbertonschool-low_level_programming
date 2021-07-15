986d87ea2c4259cd1e33412328d20ed075029dac
Sergio Vera <sergio.vera@icloud.com>
2021-07-15 20:37:52 +0000

    empty strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,8 +30,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	sum = len1 + n;
 
 	tmp = malloc(sizeof(char) * sum + 1);
-	if (tmp == NULL)
-		return (NULL);
+	if (tmp == 0)
+		return (0);
 	for (i = 0; i < len1; i++)
 		tmp[i] = s1[i];
 	for (j = 0; j < n; j++, i++)
