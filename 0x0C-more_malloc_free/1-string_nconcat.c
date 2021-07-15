283f3ab26a57a0363286d43d2893da4544e12394
Sergio Vera <sergio.vera@icloud.com>
2021-07-15 18:07:41 +0000

    removing if statemnet

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,12 +36,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		tmp[i] = s1[i];
 	}
-	for (len2 = 0; s2[len2] != '\0' && len2 < n; i++, len2++)
+	for (len2 = 0; len2 < n; i++, len2++)
 	{
-		if (n > len2)
-		{
-			tmp[i] = s2[len2];
-		}
+		tmp[i] = s2[len2];
 	}
 	tmp[i] = '\0';
 	return (tmp);
