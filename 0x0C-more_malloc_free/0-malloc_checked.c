4b13bb4d955f988ad5fc128ed55872641f51e4b6
Sergio Vera <sergio.vera@icloud.com>
2021-07-15 16:04:05 +0000

    more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * malloc_checked - allocaftes memory using malloc
+ * @b: counts elements to allocate
+ * Return: if malloc fails function should cause normal processs of termination
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	if (b == 0)
+		exit(98);
+	
+	mem = malloc(b);
+
+	if (mem == NULL)
+		exit(98);
+	
+	return (mem);
+}
