a8221c0efe95a2722fc471455fa535d8f72d681b
Sergio Vera <sergio.vera@icloud.com>
2021-07-15 20:57:41 +0000

    fixing tmp NULL

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,8 +31,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	sum = len1 + n;
 
-	tmp = malloc(sizeof(char) * sum);
-	if (tmp == 0)
+	tmp = malloc(sizeof(char) * sum + 1);
+	if (tmp == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 	{
