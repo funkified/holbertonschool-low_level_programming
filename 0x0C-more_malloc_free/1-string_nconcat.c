d0dd63e70107cbb54652c38179101c26a8711340
Sergio Vera <sergio.vera@icloud.com>
2021-07-15 17:46:30 +0000

    strncat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * string_nconcat - concanates two strings
+ * @s1: first string space in memory
+ * @s2: second string
+ * @n: byte to concat
+ * Return: new allocated space n memory which contains s1 followed by s2 n Byte
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, len1, len2, sum = 0;
+	char *tmp;
+
+	if (s1 == NULL)
+	{
+		s1 = " ";
+	}
+	if (s2 == NULL)
+	{
+		s2 = " ";
+	}
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+	if (n > len2)
+		n = len2;
+	sum = len1 + n;
+
+	tmp = malloc(sizeof(char) * sum + 1);
+	if (tmp == 0)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+	{
+		tmp[i] = s1[i];
+	}
+	if (n < len2)
+	{
+		for (len2 = 0; s2[len2] != '\0' && len2 < n; i++, len2++)
+		{
+			tmp[i] = s2[len2];
+		}
+	}
+	tmp[i] = '\0';
+	return (tmp);
+}
