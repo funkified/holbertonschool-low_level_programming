e83e37edfb0c18f6f6c7b758df0735790529722e
Sergio Vera <sergio.vera@icloud.com>
2021-07-15 18:00:43 +0000

    fixing main

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	tmp = malloc(sizeof(char) * sum + 1);
 	if (tmp == 0)
-		return (0);
+		return (NULL);
 	for (i = 0; i < len1; i++)
 	{
 		tmp[i] = s1[i];
