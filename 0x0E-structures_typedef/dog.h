a16ee3e89df9c3b1c0773f5d41d5dfa7853ac1f1
Sergio Vera <sergio.vera@icloud.com>
2021-07-19 20:31:36 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - struct with floowing element
+ * @name: name of the dog
+ * @owner: name of the dog owner
+ * @age: age of the dog
+ *
+ * Description: long description
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
