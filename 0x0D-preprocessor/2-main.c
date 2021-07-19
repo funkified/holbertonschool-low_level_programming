3fa350587b65b83d8cf4f76b982a36c195cd39ed
Sergio Vera <sergio.vera@icloud.com>
2021-07-19 19:16:25 +0000

    3 func

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - Write a program that prints the name of the file it was compiled from, followed by a new line.
+ * main - prints the name of the file it was compiled from, followed by a new line.
  * Return: nothing
  */
 
