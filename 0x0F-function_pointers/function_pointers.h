523700ac88c75c985de37da9c25b81b21ad65845
Sergio Vera <sergio.vera@icloud.com>
2021-07-22 02:04:58 +0000

    int index

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,4 +7,6 @@
 void print_name_as_is(char *name);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int *cmp(int elem);
+int int_index(int *array, int size, int (*cmp)(int));
 #endif
