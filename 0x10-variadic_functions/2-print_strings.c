2e307d424eac384dbda7fd6d59619d99b0408240
Sergio Vera <sergio.vera@icloud.com>
2021-07-22 18:57:58 +0000

    print string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints string using variadic funcitno
+ * @separator: string to be printed
+ * @n: number of strings passed to
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *ch;
+	va_list string;
+
+	va_start(string, n);
+
+	for (i = 0; i < n; i++)
+	{
+		ch = va_arg(string, char *);
+
+		if (ch == '\0')
+		{
+			ch = "(nil)";
+		}
+		printf("%s", ch);
+
+		if (ch != '\0' && i + 1 != n)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
