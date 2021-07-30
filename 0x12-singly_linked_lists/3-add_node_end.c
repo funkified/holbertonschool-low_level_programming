d6f76c317e10771240b9b0b78ca495ad40715f3c
Sergio Vera <sergio.vera@icloud.com>
2021-07-30 15:39:39 +0000

    task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds node at the end of the list
+ * @head: beggining of list
+ * @str: string to add
+ * Return: address of the new element
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *tmp;
+	int i = 0;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		;
+	if (str == NULL)
+	{
+		new->str = NULL;
+		new->len = 0;
+	}
+	else
+	{
+		for (i = 0; str[i]; i++)
+			;
+		new->str = strdup(str);
+		new->len = i;
+	}
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+	tmp->next = new;
+
+	return (new);
+}
+
