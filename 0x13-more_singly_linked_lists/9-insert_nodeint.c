56f323c238c23b6e564ba6730b16d11109f87df9
Sergio Vera <sergio.vera@icloud.com>
2021-08-04 03:28:34 +0000

    fixing loop -1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,12 +30,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp->next = temp;
 		*head = temp;
 	}
-	for (i = 1; i < idx - 1; i++)
+	for (i = 1; i < idx; i++)
 	{
 		temp = temp->next;
 	}
 	new_node->next = temp->next;
 	temp->next = new_node;
 
-	return (new_node);
+	return (new_node->next);
 }
