75beef31b4e085da2f631b3807f1c9a7daab1320
Sergio Vera <sergio.vera@icloud.com>
2021-08-04 03:58:00 +0000

    for loop into while loop

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp = *head;
 	listint_t *new_node = malloc(sizeof(listint_t));
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (head == NULL)
 	{
@@ -23,22 +23,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (0);
 	}
 	new_node->n = n;
-	new_node->next = *head;
-	*head = temp;
-	if (idx == '\0')
+	new_node->next = NULL;
+	if (idx == 0)
 	{
-		free(temp->next);
-		return (NULL);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
-	for (i = 0; i < idx - 1; i++)
+	while (i < idx - 1)
 	{
-		if (idx == 0)
-		{
-			temp->next = *head;
-			*head = temp;
-			return (new_node);
-		}
 		temp = temp->next;
+		i++;
+		if (temp == NULL)
+		{
+			free(temp);
+			return (NULL);
+		}
 	}
 	new_node->next = temp->next;
 	temp->next = new_node;
