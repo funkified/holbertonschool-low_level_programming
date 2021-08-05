786b37ff7c683204c0e80bcd5df828c6f9a12f6f
Sergio Vera <sergio.vera@icloud.com>
2021-08-05 02:08:00 +0000

    testin null

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,9 +9,9 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *curr = *head;
-	listint_t *prev = NULL;
+	listint_t *prev;
 
-	if (*head == NULL || head == NULL)
+	if (*head == NULL)
 	{
 		return (NULL);
 	}
