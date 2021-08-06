6b2439f1daff2a3a489d25c8029b5ad51dfb5c6f
Sergio Vera <sergio.vera@icloud.com>
2021-08-06 16:19:26 +0000

    task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,22 +1,21 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * clear_bit - sets the value of a bit to 0
- * @n: pointer
- * @index: index
- * Return: 1 if it worked 0 if not
+ * clear_bit - sets the value a bit to 0 at given index
+ * @index: index starting at 0
+ * @n: pointer to bit
+ * Return: 1 if it worked, -1 otherwise
  */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int i = 1;
+	unsigned long int i;
 
-	if (n == NULL || index > 32)
-		return (-1);
-
-	i = i << index;
-
-	if ((i & *n) == i)
+	if (index > 32)
 	{
-		*n -= i;
+		return (-1);
 	}
+	i = ~1 << index;
+	*n = *n & i;
 	return (1);
 }
