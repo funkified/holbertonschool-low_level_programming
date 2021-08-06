51d5eff036d4e4bc1e03b091885a9329f6b92b3c
Sergio Vera <sergio.vera@icloud.com>
2021-08-06 21:57:24 +0000

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,21 +1,24 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * flip_bits - return the # of bits you need to flip to get from one to another
- * @n: pointer
- * @m: bit to flip
- * Return: number of bits fliped
+ * flip_bits - returns the number of bits you would need to flip
+ * @n: interger
+ * @m: 2nd ints
+ * Return: number of bits flipped
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int i, y = 0;
+	unsigned int i, j = 0;
 	unsigned long int ii = sizeof(unsigned long int) * 8;
 
 	for (i = 0; i < ii; i++)
 	{
 		if ((m & 1) != (n & 1))
-			y += 1;
+		{
+			j += 1;
+		}
 		n = n >> 1;
 		m = m >> 1;
 	}
-	return (y);
+	return (j);
 }
