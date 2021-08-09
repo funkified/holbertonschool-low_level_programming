5abedec5c06eb0789c3250cbcb0ff22f21d8f785
Sergio Vera <sergio.vera@icloud.com>
2021-08-09 23:24:01 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,37 +1,45 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *create_file - create file
- *@filename: name of the file to be created
- *@text_content: NULL terminated string to write to the file
- *Return: 1 on success, -1 if fails
+ * create_file - creates a file
+ * @filename: the name of the file to create
+ * @text_content: NULL terminated string to write to file
+ * Return: 1 on succerss, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd, len, _write;
 
 	len = 0;
 
-	if (!filename)
+	if (filename == NULL)
+	{
 		return (-1);
+	}
 
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 600);
 
 	if (fd == -1)
-		return (-1);
-
-	if (text_content)
 	{
-		while (text_content[len])
-		{
-			len++;
-		}
-		_write = write(fd, text_content, len);
+		return (-1);
 	}
+	while (text_content[len])
+	{
+		if (filename != NULL)
+		{
+			return (1);
+		}
+		len++;
+	}
+	_write = write(STDOUT_FILENO, text_content, len);
+
+
 	if (_write == -1)
 	{
 		return (-1);
 	}
+
 	close(fd);
-	return (1);
+	return (fd);
 }
