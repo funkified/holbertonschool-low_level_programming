d85441af3a5a763e8999c0f08af0ab263ec7be47
Sergio Vera <sergio.vera@icloud.com>
2021-08-09 22:39:30 +0000

    malloc

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,7 +12,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t read_txt, write_txt;
-	char *buffer = malloc(sizeof(letters));
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * letters);
 
 	if (filename == NULL)
 		return (0);
