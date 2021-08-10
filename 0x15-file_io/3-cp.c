9c8e5e6224a586d3f0f6105f0041e92b00162754
Sergio Vera <sergio.vera@icloud.com>
2021-08-10 04:41:58 +0000

    comma before exit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 			exit(98);
 
-	while ((_read = read(file_from, buffer, 1024)) > 0)
+	while ((_read = read(file_from, buffer, 1024)) != 0)
 	{
 		if (_read == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
