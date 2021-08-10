61ae8e2dbc519bc3534cf5b1c93abc0a3becd59e
Sergio Vera <sergio.vera@icloud.com>
2021-08-10 04:38:58 +0000

    comma before exit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,37 +11,35 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file from file to\n");
-		exit(97);
-	file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file from file to\n"), exit(97);
+	}
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]),
+			exit(99);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	while ((_read = read(file_from, buffer, 1024)) != 0)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
+
+	while ((_read = read(file_from, buffer, 1024)) > 0)
 	{
 		if (_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+				exit(98);
 		_write = write(file_to, buffer, _read);
 		if (_write == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
+				exit(99);
 		}
 	}
 	if (close(file_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_from);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_from),
+			exit(100);
 	if (close(file_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_to);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_to),
+			exit(100);
 	return (0);
 }
