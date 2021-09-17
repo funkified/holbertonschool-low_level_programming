417afbf0e032d40076d5f4a97ace179ac4ba9ab1
Sergio Vera <sergio.vera@icloud.com>
2021-09-17 20:18:49 +0000

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print all elements in a doubly linked list
+ * @h: head node of th list
+ * Return: size of the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
