59230e633eb31991d503286485192b4998e95372
Sergio Vera <sergio.vera@icloud.com>
2021-09-17 21:12:41 +0000

    malloc after head NULL check

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,9 +8,12 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *new = NULL;
 
-	if (head == NULL || new == NULL)
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = *head;
