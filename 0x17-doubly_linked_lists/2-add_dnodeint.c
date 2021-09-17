5bd722efde269d85f5000f0496e7489e0e877b26
Sergio Vera <sergio.vera@icloud.com>
2021-09-17 21:08:40 +0000

    new node NULL check

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,11 +10,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 
+	if (head == NULL || new == NULL)
+		return (NULL);
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
-	if (new == NULL)
-		return (NULL);
 	if (head != NULL)
 	{
 		*head = new;
