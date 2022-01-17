42f32cf2ca1a95b95547ed2f0ea5589a6b3664cc
Sergio Vera <sergio.vera@icloud.com>
2022-01-17 18:50:30 +0000

    betty fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,18 +11,15 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    size_t idx;
+	size_t i;
 
-    if (array == NULL)
-        return(-1);
-
-    for (idx = 0; idx < size; idx++)
-    {
-        printf("Value checked array[%li] = [%d]\n", idx, array[idx]);
-        if (array[idx] == value)
-        {
-            return(idx);
-        }
-    }
-    return (-1);
+	if (!array)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
