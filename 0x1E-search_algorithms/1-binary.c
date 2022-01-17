0805aca9060e2f23339660b1e7c42b28b017a0f5
Sergio Vera <sergio.vera@icloud.com>
2022-01-17 16:53:06 +0000

    fixing code

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -34,7 +34,7 @@ int binary_search(int *array, size_t size, int value)
             return (mid);
         else if (array[mid] < value)
             start = mid + 1;
-        else
+        if (array[mid] > value)
             end = mid - 1;
     }
     return (-1);
