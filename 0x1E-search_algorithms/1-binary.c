02ae75b38d08dc163ca66496b6b3b81bb4ca8a90
Sergio Vera <sergio.vera@icloud.com>
2022-01-17 00:17:35 +0000

    task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+/**
+ * binary_search - seaches for a value in a sorted array of integers
+ * using Binary Search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: the indez where value is located
+ * if the value is not present in array or if array is NULL, return -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+    size_t i = 0;
+    size_t start = 0;
+    size_t end = size - 1;
+    int mid;
+
+    if (!array)
+        return (-1);
+
+    while (start <= end)
+    {
+        printf("Searching in array: ");
+        for(i = start; i <= end; i++)
+        {
+            if (i != end)
+                printf("%d, ", array[i]);
+            else
+                printf("%d,\n", array[i]);
+        }
+        mid = start + (end - start) / 2;
+        if (array[mid] == value)
+            return (array[mid]);
+        else if (array[mid] < value)
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+    return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef __SEARCH_ALGOS_H__
+#define __SEARCH_ALGOS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
