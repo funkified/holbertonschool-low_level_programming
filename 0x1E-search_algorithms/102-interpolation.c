92b95ea61693eb1660ddec1a8ee95d70fdf54fad
Sergio Vera <sergio.vera@icloud.com>
2022-01-19 23:30:29 +0000

    task 102

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - search for a value using interpolation search algorim
+ * @array: pointer to the array
+ * @size: number of elementes
+ * @value: value to search for
+ * Return: the first index where value is located, else reutnr -1
+ */
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t i, low, high, pos;
+
+	low = 0;
+	high = size - 1;
+
+	if (!array)
+		return (-1);
+
+	for (i = 0; array[high] != array[low] && value >= array[low]
+			&& value <= array[high]; i++)
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low]))
+				* (value - array[low]));
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		if (value == array[pos])
+			return (pos);
+		if (value > array[pos])
+			high = pos - 1;
+		low = pos + 1;
+		printf("Value checked array[%lu] = [%d]\n", low, array[low]);
+		if (value == array[low])
+			return (low);
+		/*	printf("Value checked array[%lu] = [%d]\n", low, array[low]);*/
+	}
+	return (-1);
+}
